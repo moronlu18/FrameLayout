47c0619b2c3074a540aee0536135dde8c688ac9a
Lourdes RM <moronlu18@gmail.com>
2024-05-20 22:53:46 +0200

    Primer Commite

---
+++ README.md
@@ -0,0 +1,18 @@
+# Ejercicio FrameLayout
+En este ejercicio se muestra una vista que tiene como layout principal
+un `FrameLayout` con un color rosa transparente. Se inserta un texto con
+una fuente personalizada mediante el recurso `/res/font`. En este caso
+las fuentes se compilan como en el archvo `R` y están disponibles en el
+sistema como un recurso más.
+
+Además modificamos la visibilidad del componente `TextView` mediante el
+atributo `android:visibility` que contiene tres valores:
+`VISIBLE`,`INVISIBLE`y `GONE`.
+
+Finalmente aplicamos la gravedad del botón respecto al componente padre,
+es decir el `FrameLayout` mediante el atributo `android:layout_gravity`.
+
+![Imagen final del ejercicicio](img/captura.png)
+
+
+
---
+++ app/src/main/java/com/moronlu18/framelayout/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.moronlu18.framelayout
+
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.moronlu18.framelayout.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.tvPhrase.visibility =
+            if (View.VISIBLE == binding.tvPhrase.visibility) View.INVISIBLE else View.VISIBLE
+    }
+}
